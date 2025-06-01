e1723861863bde13fe704e2cb53425b42f74dd1f
RibahDev <joao.ribeiro17@outlook.com>
2025-06-01 17:14:29 -0300

    Iniciando desafio de envio de formulário

---
+++ contact-form-main/script.js
@@ -0,0 +1,43 @@
+let submitForm = document.getElementById("myForm");
+
+submitForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let userFirstName = document.getElementById("firstName").value;
+  let userLastName = document.getElementById("lastName").value;
+  let userEmail = document.getElementById("email").value;
+  var selects = document.getElementsByName("user_radio");
+  var select_value;
+  for (var i = 0; i < selects.length; i++) {
+    if (selects[i].checked) {
+      select_value = selects[i].value;
+    }
+  }
+  let userMessage = document.getElementById("message").value;
+  console.log(userFirstName);
+  console.log(userLastName);
+  console.log(userEmail);
+  console.log(select_value);
+  console.log(userMessage);
+  // handle submit
+});
+alert(
+  `Dados enviados:\n
+    Nome: ${userFirstName} ${userLastName}\n
+    Email: ${userEmail}\n
+    Opção: ${selectValue}\n
+    Mensagem: ${userMessage}\n
+    Aceitou ser contatado: ${userCheck ? "Sim" : "Não"}`
+);
+
+const result = document.getElementById("result");
+result.innerHTML = `
+  <h2>Dados Enviados:</h2>
+  <p><strong>Nome:</strong> ${userFirstName} ${userLastName}</p>
+  <p><strong>Email:</strong> ${userEmail}</p>
+  <p><strong>Opção Selecionada:</strong> ${selectValue}</p>
+  <p><strong>Mensagem:</strong> ${userMessage}</p>
+  <p><strong>Aceitou ser contatado:</strong> ${userCheck ? "Sim" : "Não"}</p>
+`;
+
+console.log(submitForm);
